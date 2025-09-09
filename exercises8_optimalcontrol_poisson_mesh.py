49ca0e03422422201aecfe5d7eaea77f107035c1
oliaguan <oliayyg@outlook.com>
2025-09-09 13:32:58 +0100

    Final version

--- exercises8_optimalcontrol_poisson_mesh.py
+++ exercises8_optimalcontrol_poisson_mesh.py
@@ -12,9 +12,10 @@ Library versions:
     
     numpy - 2.3.0
     scipy - 1.15.3
-    firedrake - 2025.4.1
-    pyadjoint-ad - 2025.4.0
+    firedrake - 2025.4.2
+    pyadjoint-ad - 2025.4.1
     petsc4py - 3.23.3
+    netgen-mesher - 6.2.2504
 """
 # Create mesh using netgen
 geo = SplineGeometry()
@@ -45,7 +46,7 @@ def mark_center(mesh):
 
 
 def run(opt_package="tao_lmvm",
-        riesz_rep="L2",
+        ctrl_inner_product="L2",
         max_level=4):
     
     """
@@ -104,7 +105,6 @@ def run(opt_package="tao_lmvm",
         # j_analytic = assemble((1./2*(u_analytic-d)**2 + alpha*f_analytic**2)*dx(mesh))
 
         # Define functional of interest and the reduced functional
-        #ctrl_inner_product = "L2"
         regularisation_norm = "L2"
 
         if regularisation_norm == "L2":
@@ -119,18 +119,33 @@ def run(opt_package="tao_lmvm",
             raise ValueError("Choose control inner product as required")
 
         control = Control(f)
+
+        # dj_tol = 1e-7
+
+        # def cb(j, dj, x):
+        #     # Compute gradient norm in H 
+        #     dj = moola.DolfinDualVector(dj, inner_product=ctrl_inner_product).primal().data
+        #     dj_norm = norm(dj)
+        #     print ("Callback:\tJ = %s\t |dJ|_H = %s" % (j, dj_norm))
+
+        #     if dj_norm < dj_tol:
+        #         print("="*10 + " % s seconds " % (time.time() - start_time) + "="*10 )
+        #         print ("="*10 + " gradient norm below tolerance " + "="*10)
+        #         plot(dj, title="dj", interactive=True)
+        #         import ipdb; ipdb.set_trace()
+
         rf = ReducedFunctional(J, control)
         pause_annotation()
 
 
-        convert_options = {"riesz_representation": riesz_rep}
+        convert_options = {"riesz_representation": ctrl_inner_product}
 
         if opt_package == "tao_lmvm":
             problem = MinimizationProblem(rf)
             solver = TAOSolver(problem, {"tao_type": "lmvm",
-                                        "tao_gatol": 1.0e-6,
+                                        "tao_gatol": 1e-6,
                                         "tao_grtol": 0.0,
-                                        "tao_gttol": 0.0,
+                                        "tao_gttol": 4e-2,
                                         #"tao_monitor": None,
                                         #"tao_view": None
                                         },
@@ -146,53 +161,60 @@ def run(opt_package="tao_lmvm",
             solver = TAOSolver(problem, {"tao_type": "nls",
                                         "tao_gatol": 1.0e-6,
                                         "tao_grtol": 0.0,
-                                        "tao_gttol": 0.0,
-                                        #"tao_monitor": None,
+                                        "tao_gttol": 4e-2,
+                                        "tao_monitor": None,
                                         #"tao_view": None
                                         },
                             convert_options=convert_options)
             start_time = time.time()
             f_opt = solver.solve()
-            # solver.tao.view()
+            solver.tao.view()
             runtime = time.time() - start_time
-            iter = solver.tao.getIterationNumber()
+            # iter = solver.tao.getIterationNumber()
+            ksp = solver.tao.getKSP()
+            iter = ksp.getIterationNumber()
 
         elif opt_package == "tao_bncg":
             problem = MinimizationProblem(rf)
             solver = TAOSolver(problem, {"tao_type": "bncg",
                                         "tao_gatol": 1.0e-6,
                                         "tao_grtol": 0.0,
-                                        "tao_gttol": 0.0,
-                                        "tao_monitor": None,
-                                        "tao_view": None},
+                                        "tao_gttol": 4e-2,
+                                        #"tao_monitor": None,
+                                        #"tao_view": None
+                                        },
                             convert_options=convert_options)
             start_time = time.time()
             f_opt = solver.solve()
-            # solver.tao.view()
+            solver.tao.view()
             runtime = time.time() - start_time
             iter = solver.tao.getIterationNumber()
-
+        
         elif opt_package == "tao_ntr":
             problem = MinimizationProblem(rf)
             solver = TAOSolver(problem, {"tao_type": "ntr",
                                         "tao_gatol": 1.0e-6,
                                         "tao_grtol": 0.0,
-                                        "tao_gttol": 0.0,
+                                        "tao_gttol": 4e-2,
                                         "tao_monitor": None,
-                                        "tao_view": None},
+                                        #"tao_view": None
+                                        },
                             convert_options=convert_options)
             start_time = time.time()
             f_opt = solver.solve()
-            # solver.tao.view()
+            solver.tao.view()
             runtime = time.time() - start_time
-            iter = solver.tao.getIterationNumber()
+            # iter = solver.tao.getIterationNumber()
+            ksp = solver.tao.getKSP()
+            iter = ksp.getIterationNumber()
+            # iter = ksp.getTotalIterations()
 
         elif opt_package == "scipy":
             counter = {"nit": 0}
             def cb(x):
                 counter["nit"] += 1
             start_time = time.time()
-            f_opt = minimize(rf, tol=1.0e-6, method="L-BFGS-B", options={"maxiter": 15000, 'maxcor': 10}, callback = cb)
+            f_opt = minimize(rf, tol=1.0e-6, method="L-BFGS-B", options={"maxiter": 15000, 'maxcor': 10}, callback = cb, derivative_options={"riesz_representation":ctrl_inner_product})
             runtime = time.time() - start_time
             iter = counter["nit"]
 
@@ -232,5 +254,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     run(opt_package=args.opt,
-        riesz_rep=args.riesz,
+        ctrl_inner_product=args.riesz,
         max_level=args.max_level)
